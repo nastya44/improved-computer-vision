fc2ce63d82f7ba629b0aa4859797ca6fe05f86e1
Anton Kozyriev <sonara4mus@gmail.com>
2023-12-04 04:06:59 +0200

    feat(src/smoothing): demo section setup

--- src/smoothing/pages/4_Demo.py
+++ src/smoothing/pages/4_Demo.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from PIL import Image
+
 
 st.set_page_config(
     page_title="Gradient-free deep learning: Demo",
@@ -10,3 +12,13 @@ st.set_page_config(
 
 
 st.title("Demo")
+
+uploaded_photo = st.file_uploader("Choose a photo")
+
+if uploaded_photo is not None:
+    if uploaded_photo.type not in ["image/png", "image/jpeg"]:
+        st.error("This is not an image file (must be jpeg or png).")
+    else:
+        st.image(
+            Image.open(uploaded_photo), caption="Uploaded Image.", use_column_width=True
+        )
